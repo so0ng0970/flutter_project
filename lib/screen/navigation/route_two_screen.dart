b32c74bed0471c8a917bb5b7067b7431da8a34a5
soonger3306 <soonger3306@daum.net>
2023-03-31 21:34:00 +0900

    pushAndRemoveUntil,pushReplacement

--- lib/screen/navigation/route_two_screen.dart
+++ lib/screen/navigation/route_two_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/layout/main_layout.dart';
+import 'package:flutter_project/screen/navigation/route_three_screen.dart';
 
 class RouteTwoScreen extends StatelessWidget {
   const RouteTwoScreen({super.key});
@@ -32,6 +33,28 @@ class RouteTwoScreen extends StatelessWidget {
                 .pushNamed('/three', arguments: 'arguments : ${999}');
           },
           child: const Text('push named'),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            //   Navigator.of(context).pushReplacement()는 전에 페이지를 지워버린다. 3에서  ..pop() 쓰면 1로 이동
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(
+                builder: (_) => const RouteThreeScreen(),
+              ),
+            );
+          },
+          child: const Text(
+            'pushReplacement three',
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushNamedAndRemoveUntil(
+                '/three',
+                // 특정 라우터만 삭제 안할수 있음
+                (route) => route.settings.name == '/');
+          },
+          child: const Text('pushAndRemoveUntil three'),
         )
       ],
     );
