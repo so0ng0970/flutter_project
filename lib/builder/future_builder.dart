3d43b541134f93c294d1d18ec693ff5d03aa9849
soonger3306 <soonger3306@daum.net>
2023-04-06 16:48:55 +0900

    futureBuilder()

---
+++ lib/builder/future_builder.dart
@@ -0,0 +1,106 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class FutureBuildScreen extends StatefulWidget {
+  const FutureBuildScreen({super.key});
+
+  @override
+  State<FutureBuildScreen> createState() => _FutureBuildScreenState();
+}
+
+class _FutureBuildScreenState extends State<FutureBuildScreen> {
+  @override
+  Widget build(BuildContext context) {
+    const textStyle = TextStyle(
+      fontSize: 16.0,
+    );
+
+    return Scaffold(
+      // FutureBuilder 캐싱, 기존의 데이터를 기억을 한다.
+      body: FutureBuilder(
+        future: getNumber(),
+        // initialData: InitialData,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          // 한번도 데이터 요청을 안했을때 사용 ,
+          // if (!snapshot.hasData) {
+          //   return const Center(
+          //     child: CircularProgressIndicator(),
+          //   );
+          // }
+
+          if(snapshot.hasData){
+            // 데이터 있을때 위젯 렌더링 
+          }
+          if(snapshot.hasError){
+            // 에러가 있을때 위젯 렌더링 
+          }
+          // 로딩중일때 위젯 렌더링 
+
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  'Future Build',
+                  style: textStyle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ),
+                Text(
+                  'Constate : ${snapshot.connectionState}',
+                  style: textStyle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Text(
+                      'Data: ${snapshot.data}',
+                      style: textStyle.copyWith(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Error: ${snapshot.error}',
+                  style: textStyle.copyWith(
+                    fontWeight: FontWeight.w700,
+                    fontSize: 20.0,
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: const Text(
+                    'setState',
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Future<int> getNumber() async {
+    await Future.delayed(
+      const Duration(
+        seconds: 3,
+      ),
+    );
+    final random = Random();
+
+    // throw Exception('에러가 발생했습니다');
+
+    return random.nextInt(100);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project/builder/future_builder.dart';
 import 'package:flutter_project/screen/navigation/navigate_screen.dart';
 import 'package:flutter_project/screen/navigation/route_one_screen.dart';
 import 'package:flutter_project/screen/navigation/route_three_screen.dart';
@@ -17,16 +18,16 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // debugShowCheckedModeBanner: false, - 디버그 아이콘 없애기
-      // home: Navigatescreen(),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Navigatescreen(),
-        '/one': (context) => const RouteOneScreen(),
-        '/two': (context) => const RouteTwoScreen(),
-        '/three': (context) => const RouteThreeScreen(),
-      },
-    );
+    return const MaterialApp(
+        // debugShowCheckedModeBanner: false, - 디버그 아이콘 없애기
+        home: FutureBuildScreen()
+        // initialRoute: '/',
+        // routes: {
+        //   '/': (context) => const Navigatescreen(),
+        //   '/one': (context) => const RouteOneScreen(),
+        //   '/two': (context) => const RouteTwoScreen(),
+        //   '/three': (context) => const RouteThreeScreen(),
+        // },
+        );
   }
 }
